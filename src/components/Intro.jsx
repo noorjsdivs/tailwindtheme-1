5f5b7da5143df8facafb757097dab8c6d961e211
Noor_Mohammad <noor.jsdivs@gmail.com>
2023-07-17 22:17:22 +0600

    icon change

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -89,8 +89,13 @@ export function IntroFooter() {
   return (
     <p className="flex items-baseline gap-x-2 text-[0.8125rem]/6 text-gray-500">
       Brought to you by{' '}
-      <IconLink href="#" icon={TwitterIcon} compact large>
-        Joe Davola
+      <IconLink
+        href="https://github.com/noorjsdivs"
+        icon={GitHubIcon}
+        compact
+        large
+      >
+        Noor Mohammad
       </IconLink>
     </p>
   )
